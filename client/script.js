f3176844a6f3aeeb37e8638f4956676ac931df96
Harry moore <harrywmoore@hotmail.co.uk>
2022-03-02 20:20:25 +0000

    im feeling lucky

--- client/script.js
+++ client/script.js
@@ -1,6 +1,10 @@
 button = document.querySelector('#searchButton');
 searchedTerm = document.querySelector('#search-bar');
+LuckyButton = document.querySelector('#LuckyButton')
 const ul = document.querySelector('ul');
+
+
+
 button.addEventListener('click', function SearchResults(event) {
     let searchTerm = searchedTerm.value.toLowerCase();
     if(searchTerm === ""){
@@ -21,4 +25,17 @@ button.addEventListener('click', function SearchResults(event) {
             })
         })
     }
+})
+
+LuckyButton.addEventListener('click', function RandomResult(){
+    fetch('http://localhost:3000/search?description=shop').then(function (response) {
+        response.json().then(function (json) {
+            for(item in json){
+                for(let details = 0; details < json[item].length; details++){
+                    randomSearch = json[item][Math.floor(Math.random()*json[item].length)].Name;
+                    window.location = 'http://localhost:3000/search?description=' + randomSearch;
+                }
+            }
+            })
+        })
 })
