599af5c8a6613dfa480d5c11e1a5ab66f079ae77
Harry moore <harrywmoore@hotmail.co.uk>
2022-03-03 21:00:26 +0000

    server tests added

--- server/app.js
+++ server/app.js
@@ -11,35 +11,59 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "client")));
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get('/', (req, res) => res.sendFile("/client/index.html", {root: '..'}));
+app.get('/', (req, res) => 
+res.sendFile("/client/index.html", {root: '..'}));
+
+app.post('/', (req, res) =>{
+    res.status(405).send("Invalid method")
+})
+
+
+
+/// would like to make 404 when page doesnt exist, keeping this as it is close but not currently working.
+
+//app.get('/search', async (req, res) =>{
+//    try{
+//        const searchQuery = await req.query;
+//        const filterSearch = await searchResults.filter(description => {
+//            let isValid = true;
+//            for(key in searchQuery){
+//                let splitDescription = description[key].split(" ");
+//                let mainDescription = splitDescription[1];
+//                let subDescription = splitDescription[0];
+//               isValid = isValid && description[key] === searchQuery[key] || mainDescription === searchQuery[key] || subDescription === searchQuery[key] || description[key].includes(searchQuery[key]) || description.Name === searchQuery[key];
+//            }
+//            return isValid;
+//        });
+//        res.sendFile('/client/Search.html', {root: '..'})
+//        
+//    }
+//    catch(err){
+//        res.status(404).send(err.message);
+//    }
+//})
+
 
 app.get('/search', async (req, res) =>{
     try{
-        const searchQuery = await req.query;
-        const filterSearch = await searchResults.filter(description => {
-            let isValid = true;
-            for(key in searchQuery){
-                let splitDescription = description[key].split(" ");
-                let mainDescription = splitDescription[1];
-                let subDescription = splitDescription[0];
-                isValid = isValid && description[key] === searchQuery[key] || mainDescription === searchQuery[key] || subDescription === searchQuery[key] || description[key].includes(searchQuery[key]) || description.Name === searchQuery[key];
-            }
-            return isValid;
-        });
-        res.sendFile('/client/Search.html', {root: '..'})
-        //res.json({data: filterSearch})
+        res.sendFile('/client/Search.html', {root: '..'});
+
+}catch(err){
+        res.status(404).send("PAGE NOT FOUND");
     }
-    catch(err){
-        res.send(err.message);
-    }
-})
-module.exports = {
-    app
-  }
+});
+
+
+
 
 app.get('/api', (req, res) =>{
     res.json(searchResults);
 })
+
+
+module.exports = {
+    app
+  }
 
 
 
