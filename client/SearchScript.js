0e234d653658d48f5b983708cd8c7e02b4466f8f
Harry moore <harrywmoore@hotmail.co.uk>
2022-03-03 16:19:32 +0000

    search bar on search bar works on enter key

--- client/SearchScript.js
+++ client/SearchScript.js
@@ -1,6 +1,7 @@
 
 const section = document.getElementById('searchResult display');
 const body = document.querySelector('body');
+const searchBar = document.getElementById('search-bar');
 
 
 
@@ -15,19 +16,28 @@ fetch('http://localhost:3000/api').then(function (response) {
               if(searchedTerm === json[item].description || searchedTerm === splitDescription[0] || searchedTerm === splitDescription[1] || searchedTerm === json[item].Name || json[item].description.includes(searchedTerm) ){
                                 const node = document.createElement('a');
                                 node.href = "//" + json[item].url;
-                                alert(json[item].url)
                                 node.style.display = 'block';
                                 const text = document.createTextNode(`${json[item].Name} | ${json[item].description}`);
                                 node.appendChild(text);
                                 section.appendChild(node);
-                                
-                
-                
-                
                 
               }
             }
           })
+})
+
+
+searchBar.addEventListener('keypress', function SearchResults(event) {
+    if (event.key === 'Enter') {
+        let searchTerm = searchBar.value.toLowerCase();
+        if(searchTerm === ""){
+            alert("search input empty");
+        }
+        else{
+            window.location = 'http://localhost:3000/search?description=' + searchTerm;
+        }
+    }
+
 })
 
 
--- client/script.js
+++ client/script.js
@@ -12,20 +12,20 @@ button.addEventListener('click', function SearchResults(event) {
     }
     else{
         window.location = 'http://localhost:3000/search?description=' + searchTerm;
-        fetch('http://localhost:3000/search?description=' + searchTerm).then(function (response) {
-            response.json().then(function (json) {
-            for(item in json){
-                for(let details = 0; details < json[item].length; details++){
-                    let list = document.createElement("li")
-                    let text = document.createTextNode(`${json[item][details].Name},${json[item][details].description},${json[item][details].url}`);
-                    list.appendChild(text);
-                     ul.appendChild(list);
-                }
-            }
-            })
-        })
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 LuckyButton.addEventListener('click', function RandomResult(){
     fetch('http://localhost:3000/search?description=shop').then(function (response) {
