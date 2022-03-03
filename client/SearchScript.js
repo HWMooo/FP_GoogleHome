ed1ef8997f1d037b5d7006292768b89fd602722d
Harry moore <harrywmoore@hotmail.co.uk>
2022-03-03 16:07:31 +0000

    href links working

--- client/SearchScript.js
+++ client/SearchScript.js
@@ -14,6 +14,8 @@ fetch('http://localhost:3000/api').then(function (response) {
               let splitDescription = json[item].description.split(" ")
               if(searchedTerm === json[item].description || searchedTerm === splitDescription[0] || searchedTerm === splitDescription[1] || searchedTerm === json[item].Name || json[item].description.includes(searchedTerm) ){
                                 const node = document.createElement('a');
+                                node.href = "//" + json[item].url;
+                                alert(json[item].url)
                                 node.style.display = 'block';
                                 const text = document.createTextNode(`${json[item].Name} | ${json[item].description}`);
                                 node.appendChild(text);
